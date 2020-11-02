cac5cbac01891290c45113e89eeb76797ece3d94
nasquier <nicolas.asquier@gmail.com>
2020-11-02 19:52:09 +0100

    Add CV without formatting

---
+++ aboutme.php
@@ -0,0 +1,23 @@
+<?php  
+if(!isset($_SESSION)) { 
+    session_start(); 
+}  
+if (isset($_SESSION["user_connected"])){
+    $message = "Nice to see you, ".htmlspecialchars($_SESSION["user_connected"])."."; 
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8' />
+    <link rel='stylesheet' href='styles/main-style.css' />
+    <title>myst</title>
+</head>
+<body>
+	<?php 
+    include('parts/header.php'); 
+    include('parts/main-menu.php');
+    include('parts/cv.html');
+    ?>
+</body>
+</html>
